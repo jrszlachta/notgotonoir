186e1df875fb2c5f9cad449702a4663009a445d7
Leandro Rodrigues <lbrguitar@gmail.com>
2017-05-14 20:41:38 -0300

    Polimento dos menus, script para a cena Start Game
    Script da cena Start Game carrega valores de opções salvas
    anteriormente, ou não, caso nunca tenha jogado, pelo jogador. Menus e
    prefabs polidos

---
+++ Noir/Assets/Scripts/StartGameControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameControl : MonoBehaviour {
+
+	private PlayerPrefsInterface prefsInterface;
+
+	// Use this for initialization
+	void Start () {
+		prefsInterface.GetComponent<PlayerPrefsInterface>();
+		// AudioManager.volume = prefsInterface.GetMasterVolume(); // Set volume to the value stored previously
+	}
+}
