094b815fb7b5a4df2e11f073b961bb26d7d3d80e
Vytor Calixto <vytorcalixto@gmail.com>
2017-06-19 23:41:43 -0300

    Add pixel shader
    E som ao iniciar o jogo

--- Noir/Assets/Scripts/General/SplashFade.cs
+++ Noir/Assets/Scripts/General/SplashFade.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class SplashFade : MonoBehaviour {
 	public Image splashImage;
 	public Text splashText;
+	public Pixelate pixelate;
 	public float fadeIn = 2.5f;
 	public float fadeOut = 2.5f;
 	public float splashTime = 3f;
@@ -15,8 +16,17 @@ public class SplashFade : MonoBehaviour {
 	IEnumerator Start() {
 		splashImage.canvasRenderer.SetAlpha(0.0f);
 		splashText.canvasRenderer.SetAlpha(0.0f);
+		pixelate.lockXY = true;
+		pixelate.pixelSizeX = 7;
 
 		FadeIn();
+		yield return new WaitForSeconds(fadeIn/4);
+		pixelate.pixelSizeX = 5;
+		yield return new WaitForSeconds(fadeIn/4);
+		pixelate.pixelSizeX = 3;
+		yield return new WaitForSeconds(fadeIn/4);
+		pixelate.pixelSizeX = 1;
+		yield return new WaitForSeconds(fadeIn/4);
 		yield return new WaitForSeconds(splashTime);
 		FadeOut();
 		yield return new WaitForSeconds(fadeOut+0.5f);
